f3978fcbe79d892c4e07f96b20846287fc7d9753
ono <mutsuono14@iamas.ac.jp>
2020-05-31 10:50:41 +0900

    build

---
+++ docs/precache-manifest.26fae765130cc632855884ef7b0ab71e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460604e138b9dde319e129b1acd1b349",
+    "url": "/ed-base-proto/index.html"
+  },
+  {
+    "revision": "aec323997bbc53a2dae1",
+    "url": "/ed-base-proto/static/css/main.7900b4c5.chunk.css"
+  },
+  {
+    "revision": "295415f677f1747873fe",
+    "url": "/ed-base-proto/static/js/2.774c8142.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ed-base-proto/static/js/2.774c8142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec323997bbc53a2dae1",
+    "url": "/ed-base-proto/static/js/main.10722fc0.chunk.js"
+  },
+  {
+    "revision": "31ee8fbaee781db2b899",
+    "url": "/ed-base-proto/static/js/runtime-main.a7313ba3.js"
+  }
+]);
